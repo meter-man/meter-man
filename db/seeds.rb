0e067ffe09330bcaf43fce49e41517b8dd6640c0
Ian S. McBride <ian.s.mcb@gmail.com>
2015-11-08 21:45:15 -0500

    Separate dev and prod seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Create one user
-user_1 = User.create(
-  email: 'dev@meter-man.com',
-  password: '12345678',
-  password_confirmation: '12345678'
-)
-
-# Create one reading for the one and only user
-reading_1 = Reading.create(
-  reading: 1000,
-  reading_date: '2015-01-01',
-  created_at: '2015-01-01 [12:00:00]',
-  updated_at: '2015-01-01 [12:00:00]',
-  user_id: user_1.id
-)
+# Apply appropriate seed (dev vs. prod)
+load(Rails.root.join( 'db', 'seeds', "#{Rails.env.downcase}.rb"))
