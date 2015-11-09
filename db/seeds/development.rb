5ab1d442e53dd6b3656519d4409e37b034474b66
Ian S. McBride <ian.s.mcb@gmail.com>
2015-11-08 22:04:07 -0500

    Enhance dev seed data
    Previously dev seed data consisted of one user and one reading. Now there's one user and 10 readings.

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -7,11 +7,14 @@ user_1 = User.create(
   password_confirmation: '12345678'
 )
 
-# Create one reading for the one and only user
-reading_1 = Reading.create(
-  reading: 1000,
-  reading_date: '2015-01-01',
-  created_at: '2015-01-01 [12:00:00]',
-  updated_at: '2015-01-01 [12:00:00]',
-  user_id: user_1.id
-)
+# Create 10 reading for the one and only user
+(1..10).each do |i|
+  month = format("%02d", i)
+  Reading.create(
+    reading: 1000 * i,
+    reading_date: "2015-#{month}-01",
+    created_at: "2015-#{month}-01 [12:00:00]",
+    updated_at: "2015-#{month}-01 [12:00:00]",
+    user_id: user_1.id
+  )
+end
