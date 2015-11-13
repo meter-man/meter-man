3d6d1ee4f0173404845045b294d005da5ccfb3df
Bjorn Johnson <bjornkjohnson@gmail.com>
2015-11-12 19:56:14 -0500

    add basic cost calculation to readings

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,3 +1,11 @@
 class Reading < ActiveRecord::Base
   belongs_to :user
+
+  def self.previous_reading(user)
+    user.readings.order(:reading_date).last.reading
+  end
+
+  def self.cost_calculation(user, current_reading)
+    cost = (current_reading.to_i - previous_reading(user).to_i) * 0.28
+  end
 end
